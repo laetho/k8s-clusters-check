0a1a95b0718a1a899e074fd4026bdf9034dab2b8
a01595 <lars-erik.thorsplass@norsk-tipping.no>
2018-11-21 08:16:07 +0100

    Fail with exit code 1 when not able to connect to cluster or fetch pod information

--- cmd/compare.go
+++ cmd/compare.go
@@ -24,6 +24,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
+	"os"
 	"reflect"
 	"strings"
 )
@@ -41,7 +42,6 @@ func init() {
 	RootCmd.AddCommand(compareCmd)
 }
 
-
 func cmdCompare() {
 	glog.V(2).Infof("Number of clusters in config: %v", len(Conf.Clusters))
 	glog.V(2).Infof("Number of namespaces in config: %v", len(Conf.NameSpaces))
@@ -49,16 +49,16 @@ func cmdCompare() {
 	maps := PodMaps{}
 
 	// Get maps from all clusters
-	for _,c := range Conf.Clusters {
+	for _, c := range Conf.Clusters {
 		config := rest.Config{
-			Host : c.Url,
+			Host:        c.Url,
 			BearerToken: c.Token,
 		}
 		client := k8sclient.GetCoreRESTConfigClient(&config)
 		glog.V(2).Info(config.Host)
 		maps = append(maps, podMapFromCluster(client))
 	}
-	glog.V(3).Infof("PodMaps: %v",maps)
+	glog.V(3).Infof("PodMaps: %v", maps)
 	if len(maps) == 2 {
 		if reflect.DeepEqual(maps[0], maps[1]) {
 			glog.Info("OK, The clusters are equal")
@@ -86,20 +86,20 @@ func podMapFromCluster(c *corev1client.CoreV1Client) PodMap {
 	for _, ns := range Conf.NameSpaces {
 		lst, err := c.Pods(ns.Namespace).List(metav1.ListOptions{})
 		if err != nil {
-			glog.Warningf("Unable to fetch pods for cluster: %v", err)
-			return podmap
+			glog.Errorf("Unable to fetch pods for cluster: %v", err)
+			os.Exit(1)
 		}
 
-		for _,pod := range lst.Items {
-			if !StringInSlice(pod.Labels["deploymentconfig"],ns.Deployments) {
+		for _, pod := range lst.Items {
+			if !StringInSlice(pod.Labels["deploymentconfig"], ns.Deployments) {
 				continue
 			}
 			// build podmap, must handle multiple containers in pod
-			glog.V(2).Infof("Containers in pod, %v : %v", pod.Name ,len(pod.Spec.Containers))
+			glog.V(2).Infof("Containers in pod, %v : %v", pod.Name, len(pod.Spec.Containers))
 			for _, cnt := range pod.Spec.Containers {
-				glog.V(3).Infof("Name: %v",cnt.Name)
-				glog.V(3).Infof("Image: %v",cnt.Image)
-				imageref := strings.SplitAfterN(cnt.Image,"/", 2)
+				glog.V(3).Infof("Name: %v", cnt.Name)
+				glog.V(3).Infof("Image: %v", cnt.Image)
+				imageref := strings.SplitAfterN(cnt.Image, "/", 2)
 				podmap[ns.Namespace+"."+pod.Labels["deploymentconfig"]+"."+cnt.Name] = imageref[1]
 			}
 		}
